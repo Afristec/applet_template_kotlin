2d5e76efd9bfc646d16eb095a595db8146f9f5d2
Melissa Kobia <melissakobia@e-kraal.com>
2021-08-07 17:33:20 +0300

    Initial commit

---
+++ template/settings.gradle.kts
@@ -0,0 +1,8 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+        mavenLocal()
+    }
+}
+rootProject.name = "template"
